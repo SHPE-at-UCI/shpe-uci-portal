57ad84fe862e8431523ff830652dd8bc0bb9b0db
alejandrogznz <gonzaa12@uci.edu>
2020-02-03 00:31:25 -0800

    Resume upload input added

---
+++ app/static/js/dashboard.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    console.log("Hello");
+    let fileName = document.getElementById('resumeUpload').value;
+    document.getElementById('resumeLabel').innerHTML = fileName;
+}
