50dad89b150df8a04f5394fef06c68844da5392b
alejandrogznz <gonzaa12@uci.edu>
2020-02-03 14:28:29 -0800

    Updated function names and added TODOs

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,5 +1,11 @@
-function myFunction() {
-    console.log("Hello");
+
+// This updates the name of the file in the resume file input
+// whenever the user changes the file to upload
+// TODO: require a pdf only
+// TODO: upload to backend
+function updateFilename() { 
     let fileName = document.getElementById('resumeUpload').value;
-    document.getElementById('resumeLabel').innerHTML = fileName;
+    console.log(fileName);
+    let splitPath = fileName.split("\\");
+    document.getElementById('resumeLabel').innerHTML = splitPath[splitPath.length - 1];
 }
