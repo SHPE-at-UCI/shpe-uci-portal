09068227bd8e08d52bc8c660e4190cbd72c397cd
alejandrogznz <gonzaa12@uci.edu>
2020-02-04 15:01:55 -0800

    Added TODOs to dashboard.js

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -9,3 +9,9 @@ function updateFilename() {
     let splitPath = fileName.split("\\");
     document.getElementById('resumeLabel').innerHTML = splitPath[splitPath.length - 1];
 }
+
+// TODO: Logout.
+// Will most likely make an API call to the backend to expire the session
+// and log the user out. Redirect to the Login Page
+// TODO: Portfolio.
+// TODO: Redirect to the settings page
