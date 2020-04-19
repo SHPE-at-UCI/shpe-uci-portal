a9f9765a32998688440e610d473a0f1f64f3ab5f
Brandon Teran <bteran1005@gmail.com>
2020-04-18 20:24:08 -0700

    Added get_dict function to be used in JavaScript

--- app/routes/search.py
+++ app/routes/search.py
@@ -5,14 +5,16 @@ from flask import (
 from app.extensions import db, FBUser
 
 
-def get_all_users() -> [FBUser]:
+def get_all_users() -> [dict]:
     users_dict = db.child('users').get().val()
     users = []
 
     for uid in users_dict:
         user_info = users_dict[uid]
         fb_user = FBUser(uid, user_info)
-        users.append(fb_user)
+        user_dict = fb_user.get_dict()
+        users.append(user_dict)
+        # users.append(fb_user)
 
     return users
 
