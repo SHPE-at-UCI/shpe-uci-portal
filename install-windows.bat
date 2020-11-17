1e7d12a9876fb43ef83931a354717ac1b8811582
Josue Lopez <josueal1@uci.edu>
2020-11-16 19:03:03 -0800

    Maintain master version of install-windows.bat

--- install-windows.bat
+++ install-windows.bat
@@ -2,11 +2,8 @@ echo "Creating virtual env"
 py -3 -m venv venv 
 
 echo "Activating venv"
-<<<<<<< HEAD
+
 venv\Scripts\activate & echo "Installing Dependencies" & pip3 install -r requirements.txt & deactivate
-=======
-venv\Scripts\activate & echo "Installing Dependencies" & pip3 install -r requirements.txt & pip install -r install-pyrebase.txt & deactivate
->>>>>>> 5f38ff12eeeae4d1a9b9c6cf5d54b9e28f8ba3a6
 
 echo "Finished! Now run runapp-windows.bat"
 
