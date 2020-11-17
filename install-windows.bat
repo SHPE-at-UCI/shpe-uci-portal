1334d6f2dd6c09cd1aad3f10024125782bf6a5cc
Gfmunoz <73160574+Gfmunoz@users.noreply.github.com>
2020-11-16 19:21:44 -0800

    Update install-windows.bat

--- install-windows.bat
+++ install-windows.bat
@@ -3,7 +3,6 @@ py -3 -m venv venv
 
 echo "Activating venv"
 
-venv\Scripts\activate & echo "Installing Dependencies" & pip3 install -r requirements.txt & deactivate
 
 echo "Finished! Now run runapp-windows.bat"
 
